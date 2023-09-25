906a0ad18586543beccf35d6f7be4d53479bd302
Ben Richardson <benrichardson@Bens-Air.home>
2023-09-25 16:29:27 -0400

    updated flask

--- app.py
+++ app.py
@@ -8,11 +8,14 @@ from pymongo import MongoClient
 app = Flask(__name__)
 CORS(app)  # Enable CORS for the entire app
 
-# Connect to MongoDB
-mongo = MongoClient(port=27017)
-db = mongo.divvy_db
+# Connect to MongoDB using the MONGODB_URI environment variable
+mongo_uri = os.environ.get('MONGODB_URI')
+mongo = MongoClient(mongo_uri)
 
+# Use the desired database and collection
+db = mongo.get_default_database()
 station_names = db['station_names']
+
 # Define your Flask routes
 
 #################################################
@@ -28,14 +31,12 @@ def welcome():
     )
 
 @app.route("/api/v1.0/stations")
-@app.route("/api/v1.0/stations")
 def stations():
     """Return a list of stations from the dataset."""
-    station_cursor = db['station_names'].find()
+    station_cursor = station_names.find()
     station_data = list(station_cursor)
     return jsonify(station_data)
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 8080))
     app.run(host='0.0.0.0', port=port, debug=True)
-
