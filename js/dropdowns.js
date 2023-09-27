29c044f5f6196ec3a7eaa50cc313f27edaba9fdd
Ben Richardson <benrichardson@Bens-Air.home>
2023-09-27 00:51:50 -0400

    add dynamic dropdowns

--- js/dropdowns.js
+++ js/dropdowns.js
@@ -1,47 +1,18 @@
-// 1. Functon to fetch JSON data
-// 2. Functon to set defalt values in the collapsible__selection class
-    // Create a function that will create the html list elements for the dynamic_selection class using the html value of selectedOption0
-// Function that will allow a user to click the collapsible__header dropdown and make a selection by adding the collapsible--expanded class to the .collapsible__content. Have that selection become the new selectOption{index} value
-// 3. Function that will fetchData for the each time the selectedOption1 element is changed
+let jsonData = null;
 
-// Function to fetch JSON data
-async function fetchData() {
-    try {
-      const response = await fetch('https://divvy-db-public-5f412972abe3.herokuapp.com/api/v1.0/stations');
-      const data = await response.json();
-      return data;
-    } catch (error) {
-      console.error('Error fetching data:', error);
-    }
+async function fetchDataIfNeeded() {
+  if (!jsonData) {
+    const response = await fetch('https://divvy-db-public-5f412972abe3.herokuapp.com/api/v1.0/stations');
+    jsonData = await response.json();
+  }
 }
 
 // Function to set default values for span elements
 function setDefaultValues() {
-    document.getElementById('selectedOption0').textContent = 'ward';
+    document.getElementById('selectedOption0').textContent = 'City';
     document.getElementById('selectedOption1').textContent = 'Chicago';
     document.getElementById('selectedOption2').textContent = 'Top 10';
 }
-
-setDefaultValues();
-
-function createDynamicList() {
-    const selectedOption0Value = document.getElementById('selectedOption0').textContent;
-    const dynamicSelectionList = document.querySelector('.dynamic_selection');
-  
-    // Clear existing list items
-    dynamicSelectionList.innerHTML = '';
-  
-    // Create new list items based on selectedOption0Value
-    const listItem1 = document.createElement('li');
-    listItem1.textContent = `Option A for ${selectedOption0Value}`;
-  
-    const listItem2 = document.createElement('li');
-    listItem2.textContent = `Option B for ${selectedOption0Value}`;
-  
-    dynamicSelectionList.appendChild(listItem1);
-    dynamicSelectionList.appendChild(listItem2);
-    // Add more list items as needed based on your logic
-  }
 
 function handleCollapsibleSelection(index) {
     const collapsibleClick = document.querySelectorAll('.collapsible__selection')[index];
@@ -50,7 +21,15 @@ function handleCollapsibleSelection(index) {
   
     // Add a click event listener to the collapsible header
     collapsibleClick.addEventListener('click', function () {
-      // Toggle the 'collapsible--expanded' class to on
+      // Collapse all other collapsible elements first
+      const allCollapsibles = document.querySelectorAll('.collapsible');
+      allCollapsibles.forEach((element, i) => {
+        if (i !== index) {
+          element.classList.remove('collapsible--expanded');
+        }
+      });
+
+      // Toggle the 'collapsible--expanded' class for the clicked collapsible
       collapsible.classList.toggle('collapsible--expanded');
   
       // Check if the content is expanded
@@ -61,6 +40,9 @@ function handleCollapsibleSelection(index) {
           item.addEventListener('click', function (event) {
             // Update the selectedOption value based on the clicked item
             selectedOption.textContent = event.target.textContent;
+
+            // Set the value attribute of selectedOption0 based on the clicked item's value attribute
+            selectedOption0.setAttribute('value', event.target.getAttribute('value'));
   
             // Collapse the collapsible content by removing the 'collapsible--expanded' class
             collapsible.classList.remove('collapsible--expanded');
@@ -69,14 +51,14 @@ function handleCollapsibleSelection(index) {
   
         // Get a reference to selectedOption0
         const selectedOption0 = document.getElementById('selectedOption0');
+        const selectedOption1 = document.getElementById('selectedOption1');
 
-  
         // Initialize the MutationObserver to watch for changes in selectedOption0's textContent
         const observer = new MutationObserver(function (mutationsList) {
           for (let mutation of mutationsList) {
             if (mutation.type === 'childList' && mutation.target === selectedOption0) {
-              console.log('SelectedOption0 text content changed');
-              // Call the updateDropdownList function when selectedOption0's content changes
+              selectedOption1.textContent = '';
+              // Call the populateDynamicList function when selectedOption0 class text content changes
               populateDynamicList();
             }
           }
@@ -93,16 +75,15 @@ function handleCollapsibleSelection(index) {
   
 // Function to populate the dynamic selection list
 async function populateDynamicList() {
-
     try {
-      // Fetch JSON data
-      const jsonData = await fetchData();
+      // Fetch JSON data if not already fetched
+      await fetchDataIfNeeded();
       
       // Get the selected option text
-      const selectedOptionText = document.getElementById('selectedOption0').textContent.trim();
-      console.log(selectedOptionText)
+      const selectedOptionValue = document.getElementById('selectedOption0').getAttribute('value');
+
       // Filter the JSON data for unique values of the selected option
-      const uniqueValues = [...new Set(jsonData.map(item => item[selectedOptionText]))];
+      const uniqueValues = [...new Set(jsonData.map(item => item[selectedOptionValue]))];
       
       // Get the dynamic selection <ul> element
       const dynamicSelectionList = document.querySelector('.dynamic_selection');
@@ -121,8 +102,10 @@ async function populateDynamicList() {
     } catch (error) {
       console.error('Error populating dynamic list:', error);
     }
-  }
-  
+}
+
+setDefaultValues();
+
 // Call the function to populate the list when needed
 populateDynamicList();
   
@@ -134,4 +117,18 @@ const numCollapsibles = 3; // Adjust this as needed
 for (let index = 0; index < numCollapsibles; index++) {
   handleCollapsibleSelection(index);
 }
-  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
