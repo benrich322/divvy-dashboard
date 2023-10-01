f27cf98016303a0488bf0de3188dc550bc621ac9
Ben Richardson <benrichardson@bens-air.lan>
2023-10-01 13:47:22 -0500

    imported react components

---
+++ vite-project/src/App.tsx
@@ -0,0 +1,13 @@
+import Message from "./message";
+import Message2 from "./message2";
+import Message3 from "./message3";
+
+function App() {
+  return (
+    <div>
+      <Message3 />
+    </div>
+  );
+}
+
+export default App;
---
+++ vite-project/src/message.tsx
@@ -0,0 +1,7 @@
+function Message() {
+    // JSX: Javascript XML
+    const name = 'Mosh';
+    return <h1>Hello {name}</h1>;
+}
+
+export default Message;
