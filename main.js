e8e3b32c54cb09fd23267c7f8db05ac1aa32095f
Ben Richardson <benrichardson@bens-air.lan>
2023-10-01 21:40:12 -0500

    trying again

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var react_1 = require("react");
 var client_1 = require("react-dom/client");
-var App_1 = require("./src/App");
+var App_1 = require("./App");
 client_1.default.createRoot(document.getElementById("root")).render(<react_1.default.StrictMode>
     <App_1.default />
   </react_1.default.StrictMode>);
