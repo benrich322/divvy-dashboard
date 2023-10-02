3fb06f0f98848fee24428bcb77c4d24ebea8b7ae
Ben Richardson <benrichardson@bens-air.lan>
2023-10-01 23:20:33 -0500

    trying again

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
 import React from "react";
 import DataGridFunction from "./data_grid";
-
 function App() {
-  return (
-    <div>
-      <DataGridFunction />
-    </div>
-  );
+    return (React.createElement("div", null,
+        React.createElement(DataGridFunction, null)));
 }
-
 export default App;
