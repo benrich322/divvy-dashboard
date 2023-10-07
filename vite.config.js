9794db4f8841cb5c26dfdf532f6306657ad4e602
Ben Richardson <benrichardson@bens-air.lan>
2023-10-07 18:16:38 -0500

    udpate vite

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/react-project/', // Add this line to specify the base path
   plugins: [react()],
   build: {
     target: 'esnext', // Set this to 'esnext'
@@ -12,6 +13,7 @@ export default defineConfig({
     jsxFragment: 'React.Fragment',
   },
 });
+
 
 
 
