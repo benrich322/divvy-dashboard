3b8ccc7ca77a4a31f653b1801f23882c85364207
Ben Richardson <benrichardson@bens-air.lan>
2023-10-07 18:27:46 -0500

    updated vite.config

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/react-project/', // Add this line to specify the base path
+  base: '/divvy-dashboard/', // Add this line to specify the base path
   plugins: [react()],
   build: {
     target: 'esnext', // Set this to 'esnext'
