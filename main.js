b3c7a60b54270d12ee2b96f5e7ecc675d4d30501
Ben Richardson <benrichardson@bens-air.lan>
2023-10-08 22:39:24 -0500

    clean up js file

--- main.js
+++ main.js
@@ -22,7 +22,7 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 //    - Attribution information is provided for OpenStreetMap's usage.
 //    - The tile layer is added to the 'map' instance, making it the base layer of the map.
 
-//////// Fetch Data ////////
+//////// FUnctions to Fetch Data ////////
 
 // This code helps connect to the Heroku website that includes all the data
 
@@ -140,144 +140,130 @@ async function handleSelectionChange(map) {
 
         // Update the map header to match the selections.
         updateTitle();
-
-        //
-        //filterDataByCommunityArea();
-
-
     } catch (error) {
         // Handle any errors that occur during the process.
         console.error("Error handling selection change:", error);
     }
 }
 
-
 fetchAndCacheGeoJSONData().then(() => {
-    // Your application is now ready to use cached GeoJSON data
-    // You can access it using geojsonDataCache[location_type_selection]
+  // This block of code will run after the GeoJSON data has been fetched and cached
+  // Your application is now ready to use cached GeoJSON data
+  // You can access it using geojsonDataCache[location_type_selection]
 });
 
+//////// Functions for Map Dropdown Selections /////////
+  
+// This function helps create the dropdown user flow
+function handleCollapsibleSelection(index) {
+  // It's like, "When someone clicks on the dropdown, do some things."
 
-//// Dropdowns ////
+  // Finds the dropdown selections in index.html to click on, and it's giving it a name.
+  const collapsibleClick = document.querySelectorAll('.collapsible__selection')[index];
+  const collapsible = document.querySelectorAll('.collapsible')[index];
+  const selectedOption = document.getElementById(`selectedOption${index}`);
 
+  // When someone clicks on the dropdown, it's like, "Hey, computer, listen!"
+  collapsibleClick.addEventListener('click', function () {
 
-// This function capitalizes a word
-function capitalizeWord(word) {
-    return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
-  }
-  
-  // This function helps create the dropdown user flow
-  function handleCollapsibleSelection(index) {
-    // It's like, "When someone clicks on the dropdown, do some things."
-  
-    // First, it finds the dropdown selections in index.html to click on, and it's giving it a name.
-    const collapsibleClick = document.querySelectorAll('.collapsible__selection')[index];
-    const collapsible = document.querySelectorAll('.collapsible')[index];
-    const selectedOption = document.getElementById(`selectedOption${index}`);
-  
-    // When someone clicks on the dropdown, it's like, "Hey, computer, listen!"
-    collapsibleClick.addEventListener('click', function () {
-  
-      // First, it closes other boxes that are open.
-      const allCollapsibles = document.querySelectorAll('.collapsible');
-      allCollapsibles.forEach((element, i) => {
-        if (i !== index) {
-          element.classList.remove('collapsible--expanded');
-        }
-      });
-  
-      // Then, it opens and closes the box clicked on.
-      collapsible.classList.toggle('collapsible--expanded');
-  
-      // If the box is open, it does even more things.
-  
-      if (collapsible.classList.contains('collapsible--expanded')) {
-        // It's like, "When clicking on things inside the open box, do these things."
-  
-        const collapsibleListItems = collapsible.querySelectorAll('.collapsible__list');
-        collapsibleListItems.forEach((item) => {
-          item.addEventListener('click', function (event) {
-            // When you click on something inside the box, it changes the dropdown selection.
-  
-            selectedOption.textContent = event.target.textContent;
-  
-            // It also asks for help from another function to do more tasks.
-            handleSelectionChange(map);
-  
-            // Adding a new value attribute to the index.html
-            selectedOption.setAttribute('value', event.target.getAttribute('value'));
-            // It closes the box you clicked on by removing the collapsible--expanded class.
-            collapsible.classList.remove('collapsible--expanded');
-          });
-        });
-  
-        // It's finding the selections for the location type and location dropdowns.
-        const loction_type_selection = document.getElementById('selectedOption0');
-        const selectedOption1 = document.getElementById('selectedOption1');
-  
-        // It tells the computer to look for changes in the words.
-        const observer = new MutationObserver(function (mutationsList) {
-          for (let mutation of mutationsList) {
-            if (mutation.type === 'childList' && mutation.target === loction_type_selection) {
-              // When the words change, it does some more things and uses another function to complete a task.
-              selectedOption1.textContent = '';
-              populateDynamicList();
-            }
-          }
-        });
-  
-        // It tells the computer what kind of changes to look for.
-        const observerConfig = { childList: true };
-  
-        // It's like saying, "Hey computer, watch these words for me."
-        observer.observe(loction_type_selection, observerConfig);
+    // It closes other dropdown boxes that are open.
+    const allCollapsibles = document.querySelectorAll('.collapsible');
+    allCollapsibles.forEach((element, i) => {
+      if (i !== index) {
+        element.classList.remove('collapsible--expanded');
       }
     });
-  }
-  
-    
-  // This function helps populate the dropdown list on the web page.
-  async function populateDynamicList() {
-      try {
-        // It fetches the station data from the internet if don't have it yet.
-        await fetchStationData();
-        
-        // It finds the value attribute of the selected option of the location type dropdown
-        const selectedOptionValue = document.getElementById('selectedOption0').getAttribute('value');
-  
-        // It makes a list of unique selections and puts them on the web page.
-        const uniqueValues = [...new Set(jsonData.map(item => item[selectedOptionValue]))];
-        uniqueValues.sort((a, b) => {
-          // Check if both 'a' and 'b' are numbers
-          if (!isNaN(a) && !isNaN(b)) {
-            return a - b; // Sort numbers in ascending order
+
+    // Then, it opens and closes the box clicked on.
+    collapsible.classList.toggle('collapsible--expanded');
+
+    // If the box is open, it does even more things.
+
+    if (collapsible.classList.contains('collapsible--expanded')) {
+      // It's like, "When clicking on things inside the open box, do these things."
+
+      const collapsibleListItems = collapsible.querySelectorAll('.collapsible__list');
+      collapsibleListItems.forEach((item) => {
+        item.addEventListener('click', function (event) {
+          // When you click on something inside the box, it changes the dropdown selection.
+
+          selectedOption.textContent = event.target.textContent;
+
+          // It also asks for help from another function to do more tasks.
+          handleSelectionChange(map);
+
+          // Adding a new value attribute to the index.html
+          selectedOption.setAttribute('value', event.target.getAttribute('value'));
+          // It closes the box you clicked on by removing the collapsible--expanded class.
+          collapsible.classList.remove('collapsible--expanded');
+        });
+      });
+
+      // It's finding the selections for the location type and location dropdowns.
+      const loction_type_selection = document.getElementById('selectedOption0');
+      const selectedOption1 = document.getElementById('selectedOption1');
+
+      // It tells the computer to look for changes in the words.
+      const observer = new MutationObserver(function (mutationsList) {
+        for (let mutation of mutationsList) {
+          if (mutation.type === 'childList' && mutation.target === loction_type_selection) {
+            // When the words change, it does some more things and uses another function to complete a task.
+            selectedOption1.textContent = '';
+            populateDynamicList();
           }
-        
-          // Sort strings in alphabetical order
-          return String(a).localeCompare(String(b));
-        });
-        
-        console.log(uniqueValues);
-        // It finds the place in index.html to place the list
-        const dynamicSelectionList = document.querySelector('.dynamic_selection');
-        
-        // It clears any old things from the list.
-        dynamicSelectionList.innerHTML = '';
-        
-        // It puts the unique things in the list.
-        uniqueValues.forEach(value => {
-          const listItem = document.createElement('li');
-          listItem.textContent = value;
-          listItem.classList.add('collapsible__list');
-          dynamicSelectionList.appendChild(listItem);
-        });
-        
-      } catch (error) {
-        // If something goes wrong, it tells the computer there's a problem.
-        console.error('Error populating dynamic list:', error);
-      }
+        }
+      });
+
+      // It tells the computer what kind of changes to look for.
+      const observerConfig = { childList: true };
+
+      // It's like saying, "Hey computer, watch these words for me."
+      observer.observe(loction_type_selection, observerConfig);
+    }
+  });
 }
 
+// This function helps populate the dropdown list on the web page.
+async function populateDynamicList() {
+    try {
+      // It fetches the station data from the internet if don't have it yet.
+      await fetchStationData();
+      
+      // It finds the value attribute of the selected option of the location type dropdown
+      const selectedOptionValue = document.getElementById('selectedOption0').getAttribute('value');
+
+      // It makes a list of unique selections and puts them on the web page.
+      const uniqueValues = [...new Set(jsonData.map(item => item[selectedOptionValue]))];
+      uniqueValues.sort((a, b) => {
+        // Check if both 'a' and 'b' are numbers
+        if (!isNaN(a) && !isNaN(b)) {
+          return a - b; // Sort numbers in ascending order
+        }
+      
+        // Sort strings in alphabetical order
+        return String(a).localeCompare(String(b));
+      });
+      
+      console.log(uniqueValues);
+      // It finds the place in index.html to place the list
+      const dynamicSelectionList = document.querySelector('.dynamic_selection');
+      
+      // It clears any old things from the list.
+      dynamicSelectionList.innerHTML = '';
+      
+      // It puts the unique things in the list.
+      uniqueValues.forEach(value => {
+        const listItem = document.createElement('li');
+        listItem.textContent = value;
+        listItem.classList.add('collapsible__list');
+        dynamicSelectionList.appendChild(listItem);
+      });
+      
+    } catch (error) {
+      // If something goes wrong, it tells the computer there's a problem.
+      console.error('Error populating dynamic list:', error);
+    }
+}
 
 // This function finds the selections of location type and location
 function getSelectedOptionsText() {
@@ -314,6 +300,8 @@ function findMatchingStationsnew(selectedOptions, jsonData) {
     return matchingStations;
 }
 
+//////// Functions for Leaflet Map ////////
+
 // This function helps create the markers on a map
 function createMarkers(coordinatesArray, map) {
     // It looks at each set of coordinates in a list.
@@ -329,7 +317,6 @@ function createMarkers(coordinatesArray, map) {
     });
 }
 
-// This function helps create a border on the map.
 let geojsonDataCache = {}; // Cache for storing fetched GeoJSON data
 
 // This function helps create a border on the map.
@@ -361,9 +348,6 @@ function displayCommunityAreaBorder(selectedOptions, leafletMap) {
         displayBorderWithGeoJSON(location_type_selection, location_selection, leafletMap, geojsonDataCache[location_type_selection]);
     }
 }
-
-
-//// map.js ////
 
 // This function displays the border on the map.
 function displayBorderWithGeoJSON(location_type_selection, location_selection, leafletMap, geojsonData) {
@@ -423,6 +407,14 @@ function clearMarkersAndBorders(map) {
     });
 }
 
+//////// Misc Utility Functions ////////
+
+// This function capitalizes a word
+function capitalizeWord(word) {
+  return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+}
+
+// This function updates the title of a map element based on the content of the dropdowns
 function updateTitle() {
     // Get the text content of selectedOption0 and selectedOption1
     const selectedOption0 = document.getElementById("selectedOption0").textContent;
@@ -443,39 +435,4 @@ function updateTitle() {
         titleElement.textContent = newTitle;
     }
 }
-
-
-
-//// React.js ////
-
-function filterDataByCommunityArea(selectedOptions, reactdata) {
-    // Check if data is null or undefined before filtering
-    if (reactdata == null) {
-        return [];
-    }
-    
-    // Use the Array.prototype.filter() method to filter the data
-    const new_data = data.filter(item => {
-      // Log the value of item.community_area
-  
-      // Check the value of table_content.option0
-      switch (selectedOptions.option0) {
-        case 'city':
-          return item.city === selectedOptions.option1;
-        case 'neighborhood':
-          return item.neighborhood === selectedOptions.option1;
-        case 'community_area':
-          return item.community_area === selectedOptions.option1;
-        case 'ward':
-          return item.ward === selectedOptions.option1;
-        // Add more cases for other options if needed
-        default:
-          // If option0 doesn't match any cases, return true (no filtering)
-          return true;
-      }
-    });
-  
-    // Return the filtered data
-    return reactdata;
-  }
   
